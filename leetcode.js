156973f4abceadeb4ac5e7661db4b879a7ffa06b
Adin Kramer <fireowl@Adins-MacBook-Pro.local>
2022-07-21 04:35:31 -0600

    twoSum

--- leetcode.js
+++ leetcode.js
@@ -31,14 +31,26 @@ function longestSubstring(string) {
     return longest
 }
 
-console.log(longestSubstring('jjjjj'))
-console.log(longestSubstring('hello world'))
-console.log(longestSubstring('cabc'))
+// console.log(longestSubstring('jjjjj'))
+// console.log(longestSubstring('hello world'))
+// console.log(longestSubstring('cabc'))
 
 /* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order. */
 
 const twoSum = function(nums, target) {
-    
+    for (let i=0; i<nums.length; i++) {
+        for (let j=0; j<nums.length; j++) {
+            if (i != j) {
+                if (nums[i] + nums[j] === target) {
+                    return [i,j]
+                }
+            }
+        }
+    }
+    return 'no two numbers added up to the target!'
 };
+
+console.log(twoSum([3,2,4], 6))
+console.log(twoSum([3,2,4,5,1111], 1115))
