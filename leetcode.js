7f9aaf739f688a36e290b6f957e550a0d51ae43f
NaturalCode4 <naturalcode4@gmail.com>
2022-12-09 00:32:06 -0700

    daily temperatures js

--- leetcode.js
+++ leetcode.js
@@ -1391,3 +1391,51 @@ const evaluate = (o, stack) => {
         break
     }
 }
+
+/*
+Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+Example 1:
+
+Input: temperatures = [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
+Example 2:
+
+Input: temperatures = [30,40,50,60]
+Output: [1,1,1,0]
+Example 3:
+
+Input: temperatures = [30,60,90]
+Output: [1,1,0]
+*/
+
+var dailyTemperatures = function(temperatures) {
+    
+    //use Stack
+    //notice that stack is always in non-increasing order
+
+    //declare answers array as array of 0s equal to the length ot temperatures array
+    //declare stack
+
+    //iteration/loop from left to right until we have reached the end of the temperatures array
+        // compare current num to see if greater than top of the stack
+            // if yes, update  the answerArray[i] with differences of indecies
+            // if yes, also compare to the new top of the stack. ==> continue until not greater than top of the stack
+        // add current num to the stack
+
+    // return answer
+
+    const tempLength = temperatures.length
+    let stack = []
+    let answer = Array(tempLength).fill(0)
+
+    for (let i=0; i<temperatures.length; i++) {
+        let currentTemp = temperatures[i]
+        while (stack.length && currentTemp > stack[stack.length-1][0]) {
+            answer[stack[stack.length-1][1]] = i - stack[stack.length-1][1]
+            stack.pop()
+        }
+        stack.push([currentTemp, i])
+    }   
+    return answer
+};
